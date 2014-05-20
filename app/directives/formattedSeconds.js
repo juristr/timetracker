8998a227982b56f0c3ac67fce4058637c53e18b3
Juri <juri.strumpflohner@gmail.com>
2014-05-20 09:03:21 +0200

    shortens time humanization

--- app/directives/formattedSeconds.js
+++ app/directives/formattedSeconds.js
@@ -3,7 +3,7 @@
     'use strict';
 
 
-    var humanizeDuration = function(input) { 
+    var humanizeDuration = function(input) {
         var units = 's';
         // units is a string with possible values of y, M, w, d, h, m, s, ms
         var duration = moment().startOf('day').add(units, input),
@@ -11,9 +11,9 @@
 
         if(duration.hour() > 0){ format += "H [hours] "; }
 
-        if(duration.minute() > 0){ format += "m [minutes] "; }
+        if(duration.minute() > 0){ format += "m [min] "; }
 
-        format += " s [seconds]";
+        format += " s [secs]";
 
         return duration.format(format);
     }
